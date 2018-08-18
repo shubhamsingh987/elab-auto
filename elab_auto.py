9a5ed86a9e05fc6e68a7aa1234f37d44305cfe36
shubhamsingh987 <31474894+shubhamsingh987@users.noreply.github.com>
2018-08-18 22:08:04 +0530

    Elab_tool
    A somple code for elab program solver

--- elab_auto.py
+++ elab_auto.py
@@ -1,3 +1,4 @@
+# download chrome webdriver and extract it to the root python directory use jupyternotebooks and conda install selenium from anaconda prompt
 import time 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,10 +8,10 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
-def getcode(q_no):
+def getcode(q_no): # this fx fetches the code from a annonymous db
     driver = webdriver.Chrome()
     actions = ActionChains(driver)
-    driver.get('https://60e1d7638f78504dd5d8354197730d85.github.io/');
+    driver.get('https://60e1d7638f78504dd5d8354197730d85.github.io/'); 
     select = Select(driver.find_element_by_id('lab'))
     select.select_by_index(2) # dropdown option for ds lab
     driver.find_element_by_id("q_id").click()
@@ -32,16 +33,16 @@ driver.get('http://care.srmuniv.ac.in/rmpcseds/');
 user = driver.find_element_by_name("username")
 password = driver.find_element_by_name("password")
 user.clear()
-user.send_keys("RA1711003020466")
+user.send_keys("RA1711003020466") #enter your elab id 
 password.clear()
-password.send_keys("Awesome123")
+password.send_keys("Awesome123") # enter elab password
 driver.find_element_by_id("button").click()
 wait = WebDriverWait(driver, 10)
 element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'div.card.choice.center.indigo.darken-1')))
 driver.find_element_by_css_selector("div.card.choice.center.indigo.darken-1").click()
 driver.refresh()
-for i in range(53,55):
-    driver.get("http://care.srmuniv.ac.in/rmpcseds/login/student/code/cpp/cpp.code.php?id=1&value="+str(i+1));
+for i in range(1,100): # change the range for the problems u want
+    driver.get("http://care.srmuniv.ac.in/rmpcseds/login/student/code/cpp/cpp.code.php?id=1&value="+str(i+1)); # set id for diff rings(1,2,3)
     element=wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#codeEditor > div > div.CodeMirror-scroll')))
     WebDriverWait(driver, 5)
     element.click()
@@ -68,6 +69,7 @@ for i in range(53,55):
             WebDriverWait(driver, 10)
     except NoSuchElementException :
         pass 
-        #wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#printMsg)))
+        # remove # from below codes to add print functonality 
+        #wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#printMsg))) 
         #driver.find_element_by_id("printMsg").click()
 driver.quit() 
